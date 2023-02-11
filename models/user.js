b54bb6403f86cbec5e18be9b1fe9e38db3bf4160
SHA43F <shareefpmk44@gmail.com>
2023-02-11 21:37:38 +0530

    5.3) Users Collection

--- models/user.js
+++ models/user.js
@@ -1,16 +1,38 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const { getDb } = require("../util/database");
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(userName, email) {
+    this.userName = userName;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
